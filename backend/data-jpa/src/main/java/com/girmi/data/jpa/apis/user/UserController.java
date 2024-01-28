05832c092c9f7160718356012d382ed7c7c050ee
jooni0606 <sprbws1@gmail.com>
2024-01-28 16:19:32 +0900

    [data-jpa] search user 수정

--- backend/data-jpa/src/main/java/com/girmi/data/jpa/apis/user/UserController.java
+++ backend/data-jpa/src/main/java/com/girmi/data/jpa/apis/user/UserController.java
@@ -1,6 +1,8 @@
 package com.girmi.data.jpa.apis.user;
 
 import com.girmi.data.jpa.models.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -12,13 +14,17 @@ public class UserController {
 
     @Autowired
     UserService userService;
-
+    @Operation(description = "Search User By UserId")
     @GetMapping("/user")
     public User user(String userId) throws Exception {
         return userService.getUser(userId);
     }
 
-    @PostMapping("/userByPasswd")
+    @Operation(description = "Search User By UserId, Password", parameters = {
+            @Parameter(name = "userId", description = "User Id"),
+            @Parameter(name = "userPw", description = "User Password")
+    })
+    @GetMapping(value = "/userByUserPasswd")
     public User user(String userId, String userPw) throws Exception {
         return userService.getUserByPassWd(userId, userPw);
     }
