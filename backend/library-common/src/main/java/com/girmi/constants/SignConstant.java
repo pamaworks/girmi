1649c865a713eb8f8dd4f76f5da22e6574fce819
jooni0606 <sprbws1@gmail.com>
2024-02-18 16:35:12 +0900

    [library-common] 로그인 model 수정

--- backend/library-common/src/main/java/com/girmi/constants/SignConstant.java
+++ backend/library-common/src/main/java/com/girmi/constants/SignConstant.java
@@ -3,5 +3,6 @@ package com.girmi.constants;
 public class SignConstant {
 
     public static final String SESSION_KEY_TOKEN_JWT = "token-jwt";
+    public static final String SESSION_KEY_USERINFO = "userInfo";
 
 }
--- backend/library-common/src/main/java/com/girmi/models/User.java
+++ backend/library-common/src/main/java/com/girmi/models/User.java
@@ -1,9 +1,15 @@
 package com.girmi.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
     private String userId;
     private String userNm;
+    private String userPw;
+    private String userEmail;
 }
