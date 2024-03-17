4bf7b9257725dde36aa79b7772643ba1292a6db3
jooni0606 <sprbws1@gmail.com>
2024-03-17 16:39:08 +0900

    [typescript-vue] add loading

--- frontend/typescript-vue/src/views/contents/board/BoardList.vue
+++ frontend/typescript-vue/src/views/contents/board/BoardList.vue
@@ -4,49 +4,63 @@
     <v-col cols="12" md="12">
       <UiParentCard title="Board">
         <v-row>
-          <v-col cols="12">
-            <v-btn style="float: right" @click="useBoard.isOpenBrdTypeModal = true">Board Type</v-btn>
-            <board-type></board-type>
+          <v-col>
+            <v-row>
+              <v-col cols="12">
+                <v-btn style="float: right" @click="useBoard.isOpenBrdTypeModal = true">Board Type</v-btn>
+                <board-type></board-type>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <v-table density="compact" class="h-100" style="width: 100%">
+                  <thead>
+                    <tr>
+                      <th class="text-left">Board Type</th>
+                      <th class="text-left">Title</th>
+                      <th>Delete</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="item in useBoard.boardPaging.boardList" :key="item.brdIdx">
+                      <td>
+                        <router-link :to="{ name: 'BoardEdit', params: { brdIdx: item.brdIdx } }">
+                          {{ item.boardType.brdNm }}
+                        </router-link>
+                      </td>
+                      <td>
+                        <router-link :to="{ name: 'BoardEdit', params: { brdIdx: item.brdIdx } }">
+                          {{ item.brdTitle }}
+                        </router-link>
+                      </td>
+                      <td>
+                        <v-btn density="compact" icon="mdi-delete-circle" @click="deleteBoard(itembrdIdx)"> </v-btn>
+                      </td>
+                    </tr>
+                  </tbody>
+                </v-table>
+                <v-overlay :model-value="useBoard.loadingBoardList" class="align-center justify-center" scrim="#C0C0C0" contained>
+                  <v-progress-circular color="primary" size="64" indeterminate></v-progress-circular>
+                </v-overlay>
+              </v-col>
+            </v-row>
+            <r-row>
+              <v-col>
+                <v-pagination
+                  v-model="useBoard.param.pageNo"
+                  :length="useBoard.boardPaging.page ?? 0"
+                  rounded="circle"
+                  @update:model-value="goPage"
+                ></v-pagination>
+              </v-col>
+            </r-row>
+            <v-row>
+              <v-col>
+                <v-btn variant="outlined" class="float-right" @click="addBtn">Add</v-btn>
+              </v-col>
+            </v-row>
           </v-col>
         </v-row>
-
-        <v-table density="compact" class="h-100">
-          <thead>
-            <tr>
-              <th class="text-left">Board Type</th>
-              <th class="text-left">Title</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="item in useBoard.boardPaging.boardList" :key="item.brdIdx">
-              <td>
-                <router-link :to="{ name: 'BoardEdit', params: { brdIdx: item.brdIdx } }">
-                  {{ item.boardType.brdNm }}
-                </router-link>
-              </td>
-              <td>
-                <router-link :to="{ name: 'BoardEdit', params: { brdIdx: item.brdIdx } }">
-                  {{ item.brdTitle }}
-                </router-link>
-              </td>
-              <td>
-                <v-btn density="compact" icon="mdi-delete-circle" @click="deleteBoard(item.brdIdx)"> </v-btn>
-              </td>
-            </tr>
-          </tbody>
-        </v-table>
-        <r-row>
-          <v-col>
-            <v-pagination
-              v-model="useBoard.param.pageNo"
-              :length="useBoard.boardPaging.page ?? 0"
-              rounded="circle"
-              @update:model-value="goPage"
-            ></v-pagination>
-          </v-col>
-        </r-row>
-        <v-btn variant="outlined" class="float-right" @click="addBtn">Add</v-btn>
       </UiParentCard>
     </v-col>
   </v-row>
