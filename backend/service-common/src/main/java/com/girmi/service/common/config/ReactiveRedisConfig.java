052d1483a4747f827229bdf56147a0dc479ae809
jooni0606 <sprbws1@gmail.com>
2024-05-04 15:23:54 +0900

    [service-common] sse 추가

--- backend/service-common/src/main/java/com/girmi/service/common/config/ReactiveRedisConfig.java
+++ backend/service-common/src/main/java/com/girmi/service/common/config/ReactiveRedisConfig.java
@@ -3,6 +3,7 @@ package com.girmi.service.common.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
@@ -12,16 +13,17 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class ReactiveRedisConfig {
-  @Value("${spring.redis.host}")
+  @Value("${spring.data.redis.host}")
   private String redisHost;
 
-  @Value("${spring.redis.port}")
+  @Value("${spring.data.redis.port}")
   private int redisPort;
 
-  @Value("${spring.redis.password}")
+  @Value("${spring.data.redis.password}")
   private String redisPassword;
 
   @Bean
+  @Primary
   public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory() {
     LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisHost, redisPort);
     lettuceConnectionFactory.setPassword(redisPassword);  // Redis 접속을 위한 비밀번호 설정
